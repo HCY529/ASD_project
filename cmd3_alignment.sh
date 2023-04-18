38f418d6bed10da20fd6084587fe66438078cea5
HCY529 <118234620+HCY529@users.noreply.github.com>
2023-04-18 13:32:38 +0800

    Update cmd3_alignment.sh

--- cmd3_alignment.sh
+++ cmd3_alignment.sh
@@ -7,4 +7,3 @@ bismark  -p 4  ../reference/  ../trim_data/CTF_1_trimmed.fq
 bismark  -p 4  ../reference/  ../trim_data/CTF_2_trimmed.fq
 bismark  -p 4  ../reference/  ../trim_data/CTM_1_trimmed.fq
 bismark  -p 4  ../reference/  ../trim_data/CTM_2_trimmed.fq
-
